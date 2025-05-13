b394e7fb4beef7dad0048f0ff1173944c6fa706d
ilirkl <ilirkl@gmail.com>
2025-05-13 09:48:40 +0200

    1.2.7.2
    included vercel analytics

--- src/app/[lang]/layout.tsx
+++ src/app/[lang]/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/components/theme-provider"; // If using dark mode toggle
+import { Analytics } from "@vercel/analytics/next"
 
 
 
